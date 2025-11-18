831bf8df3e831c3d9109f1a13913f6415a28083e
Farzan <farzan.mmanafi@gmail.com>
2025-11-18 19:35:01 +0330

    refactor(queue): remove commented-out code and clean up module imports

--- src/infrastructure/queue/queue.module.ts
+++ src/infrastructure/queue/queue.module.ts
@@ -1,78 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { BullModule } from '@nestjs/bull';
-// import { ConfigModule, ConfigService } from '@nestjs/config';
-
-// import { QueueService } from './queue.service';
-// import { NotificationModule } from '../../domain/notification/notification.module';
-// import { QUEUE_NAMES } from './queue.constants';
-// import { SchedulerService } from './scheduler.service';
-// import { NotificationProcessor } from '@infrastructure/processors/notification.processor';
-// import { CleanupProcessor } from '@infrastructure/processors/cleanup.processor';
-// import { QueueController } from './queue.controller';
-// import { TodoModule } from '@domain/todo/todo.module';
-
-// @Module({
-//   imports: [
-//     // Register notification queue
-//     BullModule.registerQueueAsync({
-//       name: QUEUE_NAMES.NOTIFICATION,
-//       imports: [ConfigModule],
-//       inject: [ConfigService],
-//       useFactory: (configService: ConfigService) => ({
-//         redis: {
-//           host: configService.get('REDIS_HOST', 'localhost'),
-//           port: configService.get('REDIS_PORT', 6379),
-//           password: configService.get('REDIS_PASSWORD'),
-//         },
-//         defaultJobOptions: {
-//           attempts: 3,
-//           backoff: {
-//             type: 'exponential',
-//             delay: 1000,
-//           },
-//           removeOnComplete: true,
-//           removeOnFail: false,
-//         },
-//       }),
-//     }),
-
-//     // Register cleanup queue
-//     BullModule.registerQueueAsync({
-//       name: QUEUE_NAMES.CLEANUP,
-//       imports: [ConfigModule],
-//       inject: [ConfigService],
-//       useFactory: (configService: ConfigService) => ({
-//         redis: {
-//           host: configService.get('REDIS_HOST', 'localhost'),
-//           port: configService.get('REDIS_PORT', 6379),
-//           password: configService.get('REDIS_PASSWORD'),
-//         },
-//         defaultJobOptions: {
-//           attempts: 2,
-//           backoff: {
-//             type: 'fixed',
-//             delay: 5000,
-//           },
-//           removeOnComplete: true,
-//           removeOnFail: false,
-//         },
-//       }),
-//     }),
-
-//     NotificationModule,
-//     TodoModule,
-//   ],
-//   controllers: [QueueController],
-//   providers: [
-//     QueueService,
-//     SchedulerService,
-//     NotificationProcessor,
-//     CleanupProcessor,
-//   ],
-//   exports: [QueueService, SchedulerService],
-// })
-// export class QueueModule {}
-
 import { Module, forwardRef } from '@nestjs/common';
 import { BullModule } from '@nestjs/bull';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -135,7 +60,7 @@ import { QueueController } from './queue.controller';
     }),
 
     NotificationModule,
-    forwardRef(() => TodoModule), // Use forwardRef here
+    forwardRef(() => TodoModule),
   ],
   controllers: [QueueController],
   providers: [
